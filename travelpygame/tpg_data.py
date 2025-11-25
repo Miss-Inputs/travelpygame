0e70cfbf2aab009f64cf2091173dc5375435fbc1
Megan Leet <megan.leet@outlook.com.au>
2025-11-25 17:43:05 +1100

    tpg_data: Add option to use 5K suffix when loading from KMZ/KML

--- travelpygame/tpg_data.py
+++ travelpygame/tpg_data.py
@@ -170,7 +170,7 @@ async def get_main_tpg_rounds_with_path(
 	return rounds
 
 
-def _convert_submission_from_tracker(sub: Placemark) -> Submission:
+def _convert_submission_from_tracker(sub: Placemark, fivek_suffix: str | None = None) -> Submission:
 	extra = {}
 	if sub.style:
 		style_match = re.match(r'#(icon-.+?)-(.+?)-(.+)', sub.style)
@@ -182,11 +182,18 @@ def _convert_submission_from_tracker(sub: Placemark) -> Submission:
 			extra['colour'] = style_match[2]
 			extra['style'] = style_match[3]  # labelson, nodesc, etc
 
+	is_5k = None
+	name = sub.name
+	if fivek_suffix is not None and name.endswith(fivek_suffix):
+		name = name.removesuffix(fivek_suffix)
+		is_5k = True
+
 	return Submission(
-		name=sub.name,
+		name=name,
 		latitude=sub.point.y,
 		longitude=sub.point.x,
 		description=sub.description,
+		is_5k=is_5k,
 		**extra,  # pyright: ignore[reportArgumentType] #Pylance does not know how pydantic extra works, it seems
 	)
 
@@ -205,11 +212,13 @@ def convert_submission_tracker(
 	tracker: Path | Sequence[Path] | SubmissionTracker,
 	start_round: int = 1,
 	season: int | list[int] | None = None,
+	fivek_suffix: str | None = None,
 ) -> list[Round]:
 	"""Converts a submission tracker (paths to .kml file(s), or a SubmissionTracker already parsed from such) to this more consistent format.
 
 	Arguments:
 		season: If a list, this is a list of starting rounds for each season (assumes there is a season zero, not because I can't be bothered handling one-based indexing, but because most spinoffs have a pilot).
+		fivek_suffix: Optional string to put on the end of submission names to mark them manually as a 5K.
 	"""
 	if not isinstance(tracker, SubmissionTracker):
 		tracker = parse_submission_kml(tracker)
@@ -219,7 +228,9 @@ def convert_submission_tracker(
 		round_season = _find_season(season, i) if isinstance(season, list) else season
 		# TODO: Any scenario where is_tie would be considered True, eventually we might need to implement that
 		# TODO: Ensure that there are no duplicate names
-		subs = [_convert_submission_from_tracker(sub) for sub in tracker_round.submissions]
+		subs = [
+			_convert_submission_from_tracker(sub, fivek_suffix) for sub in tracker_round.submissions
+		]
 		rounds.append(
 			Round(
 				name=tracker_round.name,
