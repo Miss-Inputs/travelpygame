d23e80459ffc9f2b42d3d304a9fca8a46064c01d
Megan Leet <megan.leet@outlook.com.au>
2026-05-16 18:38:28 +1000

    point_set_stats: Add corner_distance and closest_to_corners, also fix find_clusters whoops

--- travelpygame/point_set_stats.py
+++ travelpygame/point_set_stats.py
@@ -18,6 +18,7 @@ from scipy.optimize import differential_evolution
 from tqdm.auto import tqdm
 
 from .util import geod_distance, get_closest_index, get_distances, get_geometry_antipode
+from .util.geom_utils import get_bbox_corners
 
 if TYPE_CHECKING:
 	from shapely.geometry.base import BaseGeometry
@@ -387,6 +388,8 @@ def find_clusters(
 	points: GeoSeries,
 	threshold: float,
 	linkage_method: Literal['single', 'complete', 'average'] = 'average',
+	*,
+	use_tqdm: bool = True,
 ):
 	"""Finds clusters using hierarchal clustering. May be slow on larger datasets.
 
@@ -399,8 +402,24 @@ def find_clusters(
 
 	size = points.index.size
 	# We can just use fclusterdata, but it's probably cleaner down the line to do it in two separate steps
-	with tqdm(desc='Clustering', total=(size * (size - 1)) / 2) as t:
+	with tqdm(desc='Clustering', total=(size * (size - 1)) / 2, disable=not use_tqdm) as t:
 		# Potentially, we want to use scipy.spatial.distance.pdist to get a distance matrix ourself, but eh, we can let linkage() do it
 		linkage_matrix = linkage(coords, linkage_method, partial(_distance_metric_func, t=t))
 		labels = fcluster(linkage_matrix, threshold, 'distance')
-	return GeoSeries(labels, index=points.index)
+	return pandas.Series(labels, index=points.index)
+
+
+def corner_distance(
+	point: shapely.Point | tuple[float, float], *corners: shapely.Point, use_haversine: bool = False
+):
+	"""Sum of distances from `point` to each corner in `corners`, intended to be the corner points of a bounding box."""
+	distances = get_distances(point, corners, use_haversine=use_haversine)
+	return distances.sum().item()
+
+
+def closest_to_corners(
+	points: GeoSeries | Collection[shapely.Point], *, use_haversine: bool = False
+) -> shapely.Point:
+	"""Returns the point in `points` that is closest to all corners of the bounding box of all of `points`, usually used to roughly determine a centre of the points."""
+	corners = get_bbox_corners(points)
+	return min(points, key=partial(corner_distance, *corners, use_haversine=use_haversine))
