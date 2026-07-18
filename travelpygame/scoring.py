c8a2618100524f3418b2a61cefaa69df2d5da109
Megan Leet <megan.leet@outlook.com.au>
2026-07-18 16:35:43 +1000

    This ty suppression is no longer needed

--- travelpygame/scoring.py
+++ travelpygame/scoring.py
@@ -98,7 +98,7 @@ def score_distances(
 	else:
 		world_distance = options.world_distance_km * 1_000
 		distance_scores = (world_distance - distances) / 1_000
-		distance_scores *= 5_000.0 / options.world_distance_km  # ty:ignore[unsupported-operator] #float / Series[float] is very much supported?
+		distance_scores *= 5_000.0 / options.world_distance_km
 	if options.clip_negative:
 		distance_scores = distance_scores.clip(0)
 
